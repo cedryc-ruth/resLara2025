a4137cf1dc2380002885494f2dff4242f0d997df
Cédryc Ruth <ceruth@epfc.eu>
2024-08-08 15:51:56 +0200

    Update templates Artist & Type

--- resources/views/artist/show.blade.php
+++ resources/views/artist/show.blade.php
@@ -5,6 +5,13 @@
 @section('content')
     <h1>{{ $artist->firstname }} {{ $artist->lastname }}</h1>
 
+    <h2>Liste des types</h2>
+    <ul>
+    @foreach($artist->types as $type)    
+        <li>{{ $type->type }}</li>
+    @endforeach
+    </ul>
+
     <div><a href="{{ route('artist.edit' ,$artist->id) }}">Modifier</a></div>
 
     <form method="post" action="{{ route('artist.delete', $artist->id) }}" onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer ?')">
