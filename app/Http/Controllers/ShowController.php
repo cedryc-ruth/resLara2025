1dfd1fe6f25b8f0112098d99f9f26d3d585cb92e
Cédryc Ruth <ceruth@epfc.eu>
2024-08-08 20:52:23 +0200

    Update templates Show (added artists list)

--- app/Http/Controllers/ShowController.php
+++ app/Http/Controllers/ShowController.php
@@ -38,8 +38,18 @@ class ShowController extends Controller
      */
     public function show(string $id)
     {
+        $show = Show::find($id);
+
+        //Récupérer les artistes du spectacle et les grouper par type
+        $collaborateurs = [];
+        
+        foreach($show->artistTypes as $at) {
+            $collaborateurs[$at->type->type][] = $at->artist;
+        }
+
         return view('show.show',[
-            'show' => Show::find($id),
+            'show' => $show,
+            'collaborateurs' => $collaborateurs,
         ]);
     }
 
