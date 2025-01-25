ad81846a4d3150a626ba11103db40e693d547498
Cédryc Ruth <ceruth@epfc.eu>
2025-01-25 20:23:50 +0100

    API RESTful with token authentication (Sanctum)

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,40 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistApiController;
+use App\Models\User;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
 
-//Route::middleware('auth:basic')->group(function () {
-    Route::apiResource('artists', ArtistApiController::class)->middleware('auth.basic');
-//});
+    $user = User::where('email', $request->email)->first();
+
+    if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    $token = $user->createToken('api-token')->plainTextToken;
+
+    return response()->json(['token' => $token], 200);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/tokens/create', function (Request $request) {
+        $token = $request->user()->createToken($request->token_name);
+
+        return ['token' => $token->plainTextToken];
+    });
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+//Route::apiResource('artists', ArtistApiController::class)->middleware('auth.basic');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('artists', ArtistApiController::class);
+});
+
